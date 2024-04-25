730044a3070f79f0c7b9bb75021b934973e5dfda
Pedro Bustamante <92451773+pedrobusta@users.noreply.github.com>
2024-04-25 12:32:36 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://pedrobusta.github.io',
-  base: '',
+  base: 'pedrobusta.github.io',
 });
